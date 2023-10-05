37c7fc0059d10c733bbd7ade172d52068f6c7337
vijeyash <vijeyash@gmail.com>
2023-10-05 22:02:58 +0530

    allowing more guessing chances

--- src/main.rs
+++ src/main.rs
@@ -2,24 +2,26 @@ use rand::Rng;
 use std::cmp::Ordering;
 use std::io;
 fn main() {
-    println!("guess something");
-    let mut guess = String::new();
-    io::stdin()
-        .read_line(&mut guess)
-        .expect("Failed to read the line");
+    loop {
+        println!("guess something");
+        let mut guess = String::new();
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read the line");
 
-    let guess: u32 = guess.trim().parse().expect("Please type a number!");
+        let guess: u32 = guess.trim().parse().expect("Please type a number!");
 
-    let secret_number = rand::thread_rng().gen_range(1..=100);
-    match guess.cmp(&secret_number) {
-        Ordering::Less => {
-            println!("You guess a lesser number");
-        }
-        Ordering::Greater => {
-            println!("You guess a greater number");
-        }
-        Ordering::Equal => {
-            println!("You Won");
+        let secret_number = rand::thread_rng().gen_range(1..=100);
+        match guess.cmp(&secret_number) {
+            Ordering::Less => {
+                println!("You guess a lesser number");
+            }
+            Ordering::Greater => {
+                println!("You guess a greater number");
+            }
+            Ordering::Equal => {
+                println!("You Won");
+            }
         }
     }
 }
